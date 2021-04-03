ecaaf6556a010b306a8c2bfa43f5f692fa49c3cb
Sudip Rai <raisudip05@gmail.com>
2021-04-03 10:47:34 +0545

    Code added for inserting room

---
+++ src/Container/Addroom.js
@@ -0,0 +1,68 @@
+import { Component } from "react"
+import axios from 'axios';
+
+class Addroom extends Component{
+    state={
+        fullname:"",
+        phone:"",
+        email:"",
+        password:""
+    }
+    addroom=()=>{
+        const data={
+            fullname:this.state.fullname,
+            phone:this.state.phone,
+            email:this.state.email,
+            password:this.state.password
+            
+        }
+        axios.post("http://localhost:90/register",data).then().catch(
+           
+        )
+    }
+    render(){
+        return(
+            <div>
+            <div class="container">
+                         <div class="form-container">
+                         <div class="form-body">
+                             <div class="header">
+                                 <h2>Register</h2>
+                                 <p>Already Registered? <a href="#">Login to your account</a></p>
+                             </div>
+                             <div class="form-group">
+                                 <div class="input-group">
+                                 <input type="text" placeholder="Fullname" value={this.state.fullname} onChange={(event)=>{this.setState({fullname:event.target.value})}}/>
+                                 </div>
+                                 <div class="input-group">
+                                 <input type="text" placeholder="Phone Number" value={this.state.phone} onChange={(event)=>{this.setState({phone:event.target.value})}} />
+                                 </div>
+                                 <div class="input-group">
+                                 <input type="email" placeholder="Email" value={this.state.email} onChange={(event)=>{this.setState({email:event.target.value})}} />
+                                 </div>
+                                 <div class="input-group">
+                                 <input type="text" placeholder="Password" value={this.state.password} onChange={(event)=>{this.setState({password:event.target.value})}} />
+                                 </div>
+                            
+                                 <div class="input-group right">
+                                 <button onClick={this.register}>Register</button>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="form-image">
+                             <div class="text">
+                                 <h2>Welcome to........! Room Finder</h2>
+                                 <img src="roomicon1.png"/>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+            
+            </div>
+        )
+    }
+}
+export default Addroom
+
+
+   
