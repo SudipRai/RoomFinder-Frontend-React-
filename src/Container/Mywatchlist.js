6372f9e7628a1aa600778510b2f3522b609c7a8b
Sudip Rai <raisudip05@gmail.com>
2021-04-04 22:09:07 +0545

    Created to view mywatchlist file and routing done

---
+++ src/Container/Mywatchlist.js
@@ -0,0 +1,13 @@
+import { Component} from "react";
+import {Route} from 'react-router-dom'
+import axios from 'axios';
+
+
+class Mywatchlist extends Component{
+    render(){
+        return(
+        )
+        }
+    }
+export default Mywatchlist;
+        
